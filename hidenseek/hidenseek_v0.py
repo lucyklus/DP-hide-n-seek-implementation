4728419cce0c15591c68e50b643d22d5da0421b1
Lucia Mandová <lucka.mandova@gmail.com>
2023-10-29 11:42:49 +0100

    :sparkles: prisoner & guard env

--- hidenseek/hidenseek_v0.py
+++ hidenseek/hidenseek_v0.py
@@ -1,4 +1,4 @@
 # imports the environment - we use the file name for environment version control
-from hidenseek.env.hidenseek import env
+from hidenseek.environment.hidenseek import env
 
 __all__ = ["env"]
